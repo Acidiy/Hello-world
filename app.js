22375e47335a234f8538a755a5212ab2fd1b3a0e
Acidiy <acidi1089@gmail.com>
2024-06-05 22:59:44 +0800

    card counter

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let section = document.getElementsByClassName('section')[0];
 let addcard = document.getElementById('pushcard');
-
+let cardcounter = document.createElement ('p');
+let ncards = 0;
 
 addcard.addEventListener('click', function() {
     let card = document.createElement('div');
     card.classList.add('card');
     section.appendChild(card);
+
+    ncards++
+    addcard.innerText = `${ncards}`;
+
     delete card;
-  });
+});
