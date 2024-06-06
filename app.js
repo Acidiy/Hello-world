0d5c16439f188844e5105aff81b07126d115b1db
Acidiy <acidi1089@gmail.com>
2024-06-06 13:58:57 +0800

    preparation for list adding and card name changing function

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 let section = document.getElementsByClassName('card-display')[0];
-const bulletpoint = document.createElement('li');
+let bulletpoint = document.createElement('li');
 
 let addcard = document.getElementById('push-card');
 let addmultiplecards = document.getElementById('mass-card-push');
 
 let ncards = 0;
 let cardid = 1;
+let headerid = 1;
+let cardbuttonaddlistid = 1;
+let cardchangenamebuttonid = 1;
+let idgiver = 1;
 
 
 const cardcreator = () =>
     {
         let headerofcard = document.createElement('div');
+        let cardbuttonaddlist = document.createElement('button');
+        let cardchangenamebutton = document.createElement('button');
         let card = document.createElement('div');
         
         headerofcard.classList.add('header-of-card');
+        headerofcard.id = `headerid${idgiver}`;
+        cardbuttonaddlist.className = 'card-control-panel-button-add-list';
+        cardbuttonaddlist.id = `cardbutton-add-list-id${idgiver}`;
+        cardchangenamebutton.className = 'card-control-panel-button-change-name';
+        cardchangenamebutton.id = `card-change-name-button-id${idgiver}`;
         card.classList.add('card');
 
+        headerofcard.appendChild(cardbuttonaddlist);
+        headerofcard.appendChild(cardchangenamebutton);
         card.appendChild(headerofcard);
-        card.id = `card+" "${cardid}`;
-        cardid++;
+        card.id = `card${idgiver}`;
 
+        idgiver++;
         delete headerofcard;
+        delete cardbutton;
 
         return card;
     }
@@ -40,9 +54,9 @@ addcard.addEventListener('click', function() {
     cardpusher()
 });
 
- addmultiplecards.addEventListener('click', function() {
-     let amountofcards = prompt("how many cards?");
-     for (let i = 0; i<amountofcards; i++)
+addmultiplecards.addEventListener('click', function() {
+    let amountofcards = prompt("how many cards?");
+    for (let i = 0; i<amountofcards; i++)
         {
             cardpusher()
         }
